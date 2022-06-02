ad19d20e8e62a8284ad98694a5caafeb0ed6c027
ecsbeats <adamb@csxlabs.org>
2022-06-01 20:21:22 -0700

    lay out objectives

---
+++ client/index.js
@@ -0,0 +1,14 @@
+/*
+Objective: Create a full duplex connection between client and server.
+ */
+
+// Connect to server
+// Receive messages
+// Send messages
+
+// Runtime
+function main() {
+    return null;
+}
+
+main();
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+/*
+Objective: Create a full duplex connection between a client and server.
+*/
+
+// Store connections
+// Handle incoming connections
+// Receive messages
+// Send messages
+
+// Runtime
 #[tokio::main]
 async fn main() {
     println!("Hello, tokio?");
