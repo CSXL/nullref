fb8f81f350738f37f360e106294e41413046c97f
ecsbeats <adamb@csxlabs.org>
2022-06-03 21:12:48 -0700

    get a simple websocket working

--- client/index.js
+++ client/index.js
@@ -6,12 +6,21 @@ Objective: Create a full duplex connection between client and server.
 const HOST = "127.0.0.1";
 const PORT = 8080;
 
-const server = new WebSocket("ws://" + HOST + PORT.toString());
+const server = new WebSocket("ws://" + HOST + ":" + PORT.toString());
 
 server.addEventListener('open', function (event) {
     server.send('Discovered.');
 });
 
-socket.addEventListener('message', function (event) {
+server.addEventListener('message', function (event) {
     console.log('Message from server ', event.data);
 });
+
+server.addEventListener('close', function (event_) {
+    console.log('Connection closed');
+});
+
+const button = document.getElementById('button')
+button.addEventListener('click', function () {
+    server.send('Discovered.')
+})
--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@
 Objective: Create a full duplex connection between a client and server.
 */
 
+use log::{info, trace};
+use env_logger::Env;
 use std::{
     collections::HashMap,
     env,
     io::Error as IoError,
     net::SocketAddr,
-    sync::{Arc, Mutex}
+    sync::{Arc, Mutex},
 };
-use std::io::Stdout;
-use log::{trace, info, warn, error};
 
 use futures_channel::mpsc::{unbounded, UnboundedSender};
 use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
@@ -30,48 +30,52 @@ async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, peer_addres
         .expect("Error during the websocket handshake");
     info!("Websocket connection established with {}", peer_address);
 
-    let (ws_sink, ws_stream) = unbounded();
-    peer_map.lock()
-        .unwrap()
-        .insert(peer_address, ws_sink);
+    let (tx, rx) = unbounded();
+    peer_map.lock().unwrap().insert(peer_address, tx);
 
     let (peer_sink, peer_stream) = ws_stream.split();
 
-    let broadcast_incoming = peer_stream.try_for_each(|message| {
-        trace!("Received a message from {}: {}", peer_address, message.to_text().unwrap());
+    let broadcast_incoming = peer_stream.try_for_each(|message: Message| {
+        info!(
+            "Received a message from {}: {}",
+            peer_address,
+            message.to_text().unwrap()
+        );
         let locked_peers = peer_map.lock().unwrap();
 
-        let broadcast_recepeints = locked_peers.iter()
+        let broadcast_recepeints = locked_peers
+            .iter()
             .filter(|(address, _)| address != &&peer_address)
-            .map(|_, ws_sink| ws_sink);
+            .map(|(_, ws_sink)| ws_sink);
 
         for recipient in broadcast_recepeints {
             recipient.unbounded_send(message.clone()).unwrap();
         }
 
-        let _ = future::ok(());
+        future::ok(())
     });
 
-    let receive_from_peers = ws_stream.map(Ok).forward(peer_sink);
+    let receive_from_peers = rx.map(Ok).forward(peer_sink);
 
     pin_mut!(broadcast_incoming, receive_from_peers);
-    let _ = future::select(broadcast_incoming, receive_from_peers);
+    future::select(broadcast_incoming, receive_from_peers).await;
 
     info!("{} disconnected", &peer_address);
-    peer_map.lock()
-        .unwrap()
-        .remove(&peer_address);
+    peer_map.lock().unwrap().remove(&peer_address);
     trace!("{} removed from peer map", &peer_address);
 }
 
 #[tokio::main]
-async fn main() -> Result<(), IoError>{
-    env_logger::init();
+async fn main() -> Result<(), IoError> {
+    env_logger::Builder::from_env(
+        Env::default()
+            .default_filter_or("info")
+    ).init();
     let host_address = env::args()
         .nth(1)
         .unwrap_or_else(|| "127.0.0.1:8080".to_string());
 
-    let state = PeerMap::new(Mutex::new(Hashmap::new()));
+    let state = PeerMap::new(Mutex::new(HashMap::new()));
 
     let try_socket = TcpListener::bind(&host_address).await;
     let listener = try_socket.expect("Failed to bind to socket");
@@ -80,4 +84,6 @@ async fn main() -> Result<(), IoError>{
     while let Ok((stream, peer_address)) = listener.accept().await {
         tokio::spawn(handle_connection(state.clone(), stream, peer_address));
     }
+
+    Ok(())
 }
