bb4c5484ce84b33adce04690d8b6e3430beee81f
ecsbeats <adamb@csxlabs.org>
2022-06-04 18:37:02 -0700

    add yarn and eslint

--- client/index.js
+++ client/index.js
@@ -1,24 +1,51 @@
 'use strict';
+class Server extends WebSocket {
+  constructor(host, port) {
+    super('ws://' + host + ':' + port.toString());
+    return this;
+  }
+  onOpen(fn) {
+    this.addEventListener('open', fn);
+  }
+  onClose(fn) {
+    this.addEventListener('close', fn);
+  }
+  onError(fn) {
+    this.addEventListener('error', fn);
+  }
+  onMessage(fn) {
+    this.addEventListener('message', fn);
+  }
+  send(message) {
+    super.send(message);
+  }
+}
+
 const HOST = '127.0.0.1';
 const PORT = 8080;
 const CLIENT_ID = Math.floor(Math.random() * 1000);
 
-const server = new WebSocket('ws://' + HOST + ':' + PORT.toString());
+const server = new Server(HOST, PORT);
 
-server.addEventListener('open', function () {
-    console.log('Established a connection with: ' + server.url)
-    server.send('Client ID: ' + CLIENT_ID);
+server.onOpen(function() {
+  console.log('Established a connection with: ' + server.url);
+  server.send('Client ID: ' + CLIENT_ID);
 });
 
-server.addEventListener('message', function (event) {
-    console.log('Message from server ', event.data);
+server.onClose(function() {
+  console.log('Connection closed.');
 });
 
-server.addEventListener('close', function () {
-    console.log('Connection closed');
+server.onError(function(error) {
+    console.log('An error occurred: ', error);
 });
 
-const button = document.getElementById('button')
-button.addEventListener('click', function () {
-    server.send('Discovered.')
-})
+server.onMessage(function(event) {
+  console.log('Message from server: ', event.data);
+});
+
+
+const button = document.getElementById('button');
+button.addEventListener('click', function() {
+  server.send('Discovered.');
+});
