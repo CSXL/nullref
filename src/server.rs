161d21e19d0505cb99a1f7d66e9ef831aa819ae6
ecsbeats <adamb@csxlabs.org>
2022-08-09 12:25:53 -0700

    refactor

--- src/server.rs
+++ src/server.rs
@@ -1,106 +1,110 @@
-#[allow(clippy::module_inception)]
-mod server {
-    use futures_channel::mpsc::UnboundedSender;
-    use log::{info, trace};
-    use std::collections::HashMap;
-    use std::net::SocketAddr;
-    use std::sync::{Arc, Mutex};
-    use tokio::net::{TcpListener, TcpStream};
-    use tokio::task::JoinHandle;
-    use tungstenite::Message;
-    use url::Url;
+use futures_channel::mpsc::UnboundedSender;
+use log::{info, trace};
+use std::collections::HashMap;
+use std::net::SocketAddr;
+use std::sync::{Arc, Mutex, RwLock};
+use tokio::net::{TcpListener, TcpStream};
+use tokio::task::JoinHandle;
+use tungstenite::Message;
+use url::Url;
 
-    type Tx = UnboundedSender<Message>;
-    pub type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
+type Sender = UnboundedSender<Message>;
+pub type PeerMap = Arc<Mutex<HashMap<SocketAddr, Sender>>>;
 
-    #[derive(Debug)]
-    pub struct Server {
-        // Fields are options so they can temporarily be None
-        pub address: Option<String>,
-        accept_connections_task: Option<JoinHandle<()>>, // to allow closing the server listener
+#[derive(Debug)]
+pub struct Connection {
+    address: SocketAddr,
+    tx: Sender
+}
+#[derive(Debug)]
+pub struct Server {
+    // Fields are options so they can temporarily be None
+    pub address: Option<String>,
+    accept_connections_task: Option<JoinHandle<()>>, // to allow closing the server listener
+}
+
+pub type ServerPtr = Arc<RwLock<Server>>;
+
+impl Server {
+    #[allow(dead_code)] // Remove once rest of module is implemented
+    pub fn new() -> Self {
+        Self {
+            address: None,
+            accept_connections_task: None,
+        }
     }
 
-    impl Server {
-        #[allow(dead_code)] // Remove once rest of module is implemented
-        pub fn new() -> Self {
-            Self {
-                address: None,
-                accept_connections_task: None,
-            }
+    #[allow(dead_code)] // Remove once rest of module is implemented
+    pub async fn start<'a>(&mut self, address: &'a str) -> Result<(), &'a str> {
+        info!("Starting server.");
+        let result = Self::validate_address(address);
+        if result.is_err() {
+            let error = result.err().unwrap();
+            return Err(error);
+        }
+        let address = String::from(address);
+        self.address = Some(address.clone());
+        trace!("Binding to address provided.");
+        let listener = self.bind(address).await;
+        trace!("Binding successful.");
+        info!("Server started.");
+        let accept_connections_task = tokio::spawn(async {
+            Self::accept_connections(listener).await;
+        });
+        self.accept_connections_task = Some(accept_connections_task);
+        Ok(())
+    }
+
+    #[allow(dead_code)] // Remove once rest of module is implemented
+    fn validate_address(address: &str) -> Result<(), &str> {
+        let url_parse_result = Url::parse(&*("https://".to_owned() + address));
+        if url_parse_result.is_err() {
+            return Err(
+                "Invalid address passed to server.start. Example valid address: \
+            127.0.0.1:8080",
+            );
         }
 
-        #[allow(dead_code)] // Remove once rest of module is implemented
-        pub async fn start<'a>(&mut self, address: &'a str) -> Result<(), &'a str> {
-            info!("Starting server.");
-            let result = Self::validate_address(address);
-            if result.is_err() {
-                let error = result.err().unwrap();
-                return Err(error);
-            }
-            let address = String::from(address);
-            self.address = Some(address.clone());
-            trace!("Binding to address provided.");
-            let listener = self.bind(address).await;
-            trace!("Binding successful.");
-            info!("Server started.");
-            let accept_connections_task = tokio::spawn(async {
-                Self::accept_connections(listener).await;
-            });
-            self.accept_connections_task = Some(accept_connections_task);
-            Ok(())
+        let url = url_parse_result.unwrap();
+        if url.port() == None {
+            return Err("Port not specified in address. Example valid address: 127.0.0.1:8080");
+        } else if url.port().unwrap() < 1024 {
+            return Err(
+                "Port numbers below 1024 (excluding special cases) are privileged and \
+            non-writable by users. More information: \
+            https://www.w3.org/Daemon/User/Installation/PrivilegedPorts.html",
+            );
         }
+        Ok(())
+    }
 
-        #[allow(dead_code)] // Remove once rest of module is implemented
-        fn validate_address(address: &str) -> Result<(), &str> {
-            let url_parse_result = Url::parse(&*("https://".to_owned() + address));
-            if url_parse_result.is_err() {
-                return Err(
-                    "Invalid address passed to server.start. Example valid address: \
-                127.0.0.1:8080",
-                );
-            }
+    #[allow(dead_code)] // Remove once rest of module is implemented
+    async fn bind(&mut self, address: String) -> TcpListener {
+        let try_socket = TcpListener::bind(&address).await;
+        let listener = try_socket.expect("Failed to bind to socket");
+        listener
+    }
 
-            let url = url_parse_result.unwrap();
-            if url.port() == None {
-                return Err("Port not specified in address. Example valid address: 127.0.0.1:8080");
-            } else if url.port().unwrap() < 1024 {
-                return Err(
-                    "Port numbers below 1024 (excluding special cases) are privileged and \
-                non-writable by users. More information: \
-                https://www.w3.org/Daemon/User/Installation/PrivilegedPorts.html",
-                );
-            }
-            Ok(())
+    pub async fn accept_connections(listener: TcpListener) {
+        let connections = PeerMap::new(Mutex::new(HashMap::new()));
+        while let Ok((tcp_stream, address)) = listener.accept().await {
+            tokio::spawn(Self::handle_connection(
+                tcp_stream,
+                address,
+            ));
         }
+    }
 
-        #[allow(dead_code)] // Remove once rest of module is implemented
-        async fn bind(&mut self, address: String) -> TcpListener {
-            let try_socket = TcpListener::bind(&address).await;
-            let listener = try_socket.expect("Failed to bind to socket");
-            listener
-        }
+    async fn handle_connection(
+        tcp_stream: TcpStream,
+        address: SocketAddr,
+    ) {
+        // TODO: Write logic for handling connections
+    }
 
-        pub async fn accept_connections(listener: TcpListener) {
-            let connections = PeerMap::new(Mutex::new(HashMap::new()));
-            while let Ok((tcp_stream, address)) = listener.accept().await {
-                tokio::spawn(Self::handle_connection(
-                    tcp_stream,
-                    address,
-                ));
-            }
-        }
-
-        async fn handle_connection(
-            tcp_stream: TcpStream,
-            address: SocketAddr,
-        ) {
-            // TODO: Write logic for handling connections
-        }
-
-        pub async fn close(&mut self) {
-            if self.accept_connections_task.is_some() {
-                self.accept_connections_task.as_ref().unwrap().abort();
-            }
+    pub async fn close(&mut self) {
+        if self.accept_connections_task.is_some() {
+            self.accept_connections_task.as_ref().unwrap().abort();
         }
     }
 }
@@ -112,7 +116,6 @@ mod tests {
     use super::*;
     use rand as random;
     use rand::Rng as random_number_generator;
-    use server::Server;
 
     fn generate_random_address() -> String {
         let random_number = random::thread_rng().gen_range(1024..9999); // See https://en.wikipedia.org/wiki/List_of_TCP_and_UDP_port_numbers
